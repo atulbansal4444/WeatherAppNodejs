5ff7c1613f9f5a5b23cefbd16e9fda7a19f8875a
Atul Bansal <atulbansal4444@gmail.com>
2024-03-22 21:26:07 +0530

    added js and node js logic

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
+const http = require('http');
+const fs = require('fs');
+const requests = require('requests');
 
 const apiKey = '097614be0d44876b5346687cd8f9420f';
-const cityName = 'Patiala'
+const cityName = 'Patiala';
 
-const api = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+const api = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
+
+const htmlContent = fs.readFileSync("home.html", "utf-8");
+
+const replaceValue = (tempVal, realVal) => {
+    // console.log(realVal);
+    let temperature = tempVal.replace("{%temp%}", realVal.main.temp);
+    temperature = temperature.replace("{%tempmin%}", realVal.main.temp_min);
+    temperature = temperature.replace("{%tempmax%}", realVal.main.temp_max);
+    temperature = temperature.replace("{%location%}", realVal.name);
+    temperature = temperature.replace("{%country%}", realVal.sys.country);
+    temperature = temperature.replace("{%tempstatus}", realVal.weather[0].main);
+
+    return temperature;
+}
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        requests(api)
+            .on('data', (chunk) => {
+                const jsonObj = JSON.parse(chunk);
+                const arrData = [jsonObj];
+                const realTimeData = arrData
+                    .map(val => replaceValue(htmlContent, val))
+                    .join(""); // .join("") using to convert data to string.
+                res.write(realTimeData);
+            })
+            .on('end', (err) => {
+                if (err) {
+                    return console.log('connection closed due to errors', err);
+                }
+                res.end();
+            });
+    }
+});
+
+server.listen("8000", "127.0.0.1");
